840e42bc0a403a472c77553ff1f8cfbb56ac084e
maks <max.iw15@yandex.ru>
2023-02-04 10:55:41 +0300

    fiinish

---
+++ orm_migrations/school/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    obj_list = Student.objects.prefetch_related('teacher')
+    context = {
+        'object_list': obj_list
+    }
+
+    ordering = 'group'
+
+    return render(request, template, context)
